8e38eaaa4d51e7cdd1d94a6eb4d903b383e50ed7
ravi <ravi@lalita>
2023-08-18 10:32:20 -0400

    design2 still has issues ... too few turns

--- circuit/design.js
+++ circuit/design.js
@@ -1,3 +1,76 @@
+function design2() {
+  // Another way to examine desing of Magnetron Transformer on Miele
+
+ const prim  = {
+       n   : 255 ,     // no. of turns
+       awg : 26,      // winding size
+       d   : 0.483,  // wire diameter in mm
+       nTA : 4.15,   // no. of turns per sq.mm
+       lT  : 1,    // lenght of conductor in one turn
+       V   : 240,  // RMS Voltage (V)
+       I   : 3,  // RMS Current (Amp)
+  };
+     
+ const sec  = {
+       n   : 1740 ,   // no. of turns
+       awg : 30,    // winding size (code)
+       d   : 0.296, // wire diameter in mm
+       nTA : 8.81,  // no. of turns per mm2
+       lT  : 1,   // lenght of conductor in one turn
+       V   : 1680, // RMS Voltage (V)
+       I   : 0.3,  // RMS Current (amp)
+  } ;
+
+ const core  = {
+        tw:  30,  // Toungue width (mm)
+        td:  70,  // Toungue depth (mm) laminations depth)
+        ww:  15, //  Window width (mm) any one of the windows)
+        wh:  40, //  Window height (mm)
+        B:  1.0,  //  Magnetic Flux (Web/m2)
+ };
+
+
+const T = {p: prim,  s: sec, c: core,  
+           f:     50  ,   // frequency
+           pwr:   900 ,  // Rated power (W)
+           eff:   0.87,  // Efficiency
+           tpv:   1,    // Turns per volt
+}
+
+console.log("TStart", T) ;
+
+// Get currents in both windings
+T.p.I = (T.pwr / T.eff) / T.p.V ;  // amps
+T.s.I = T.pwr / T.s.V ;
+
+// Estimate Core area (Tounge) Required 
+var Acore = 1.152e2 * (T.pwr)^0.5 ;  // mm2
+
+//Calculate Number of Turns per Volt paameter
+T.tpv = 1.0 / (4.44e-6 * T.f * Acore * T.c.B ) ;
+console.log(T.tpv , T.p.V, T.tpv * T.p.V);
+
+// And Number of Turns of primary and secondary
+T.p.n = T.p.V  *  T.tpv ;
+T.s.n = T.s.V * T.tpv * 1.03 ;  // Extra to allow for copper losses
+
+// Estimate window area required
+var Acopper = (T.p.n / T.p.nTA) + (T.s.n / T.s.nTA) ;  // mm2
+var Awindow = Acopper * 1.03 ;  // Allows for insulation etc.
+
+// Assuming Tounge depth and window height are given
+// Calculate Tounge and window widths ... keeps the comparison
+// between estimate and actual simple
+//
+T.c.tw = Acore / T.c.td ;
+T.c.ww = Awindow / T.c.wh ;
+
+console.log("TDesign", T) ;
+
+} // end design2
+
+function design1()
+{
 /*
   * Lots of doubts on definitions of window, core  
   * and associated properties ... without any reference
@@ -62,3 +135,5 @@ Q = 2.22 * f * Bm * kw * Delta * Awin * Acore * 1e-6 * 1e-3 ;
 console.log({Q:Q, Wwin:Wwin, Hwin:Hwin, Awin:Awin, 
              Acore:Acore, PcondArea:PcondArea, 
 	     Delta:Delta, Emft:Emft, d:d, D:D});
+}
+
