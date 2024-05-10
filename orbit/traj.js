f6e8288260622de5dc1dd3a1e1ca3c311587dc1b
Ravi <ravi@kiran.local>
2024-05-10 13:20:38 +1000

    moving to chart.js

--- orbit/traj.js
+++ orbit/traj.js
@@ -1,36 +1,44 @@
 function plotTraj() {
 
- var traces = [] ;
+ var traces=Array(4) ;
  var params = {alpha: 0.5,  beta: [10, 2, 1, 0.45]} ;
 
-  for (i=0 ; i < 4 ; i++) {
-    console.log(params.beta[i]);
+  for (var i=0 ; i < 4 ; i++) {
     var data = orbit (params.alpha, params.beta[i]) ;
-    traces.push (  {
+    traces[i] = {
       r: data.r,
       theta: data.theta,
       mode: 'lines',
       line: {color: 'blue'},
       name: 'Subcardioid',
-      type: 'scatterpolar' });
+      type: 'scatterpolar' };
   }
 
-
   var layout = {
-    grid: {rows: 2, columns: 2, pattern: 'independent'},
-    autosize: true
+    grid: {rows: 2, columns: 2 },
   };
 
-  Plotly.newPlot('plotly', traces, layout);
+  var a = traces[0] ; var b = traces[1] ;
+  Plotly.newPlot('plotly', [b,b,b,b] );
+
 }
 
+const config = {
+  type: 'scatter',
+  data: data,
+  options: {
+    scales: {
+      x: { type: 'linear', position: 'bottom' }
+    }
+  } // end options
+};
 
 function orbit(alpha, beta) {
   // return descritized orbit data points
   // for specified parameters
    var theta = [] ;  var  r = [] ;
-   for (i=0 ; i < 100 ; i++) {
-     let phi = (i*2*Math.pi/100) ;
+   for (var i=0 ; i < 100 ; i++) {
+     let phi = (i*2*Math.PI/100) ;
      theta.push (phi) ;
      r.push (1.0 / (beta + alpha * Math.cos(phi))) ;
    }
