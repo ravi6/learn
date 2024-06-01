e8e007765575de103eed65757673b4ce10a6384b
Ravi <ravi@kiran.local>
2024-06-01 19:00:37 +1000

    finally getting somewhere with tensorflow.js

--- tflow/ex3.js
+++ tflow/ex3.js
@@ -1,14 +1,21 @@
-async function getTrainData() {
-  const response = await fetch("fashion/train.json");
+//Author: Ravi Saripalli
+//1st Jun 2024
+
+async function getData(fname) {
+  const response = await fetch(fname);
   data = await response.json();
   x = [] ; y = [] ;
-  await data.forEach ( (obj) => { y.push (obj.label) ;
+  await data.forEach ( (obj) => { 
+    			    nLabels = 10 ;
+                            v = new Array(nLabels).fill(0) ;
+                            v[obj.label] = 1 ;
+                            y.push (v) ;      //This is oneShot Labelling 
                             x.push (obj.image) 
                           } );
 
   data =  {imgs: x , lbls: y}  ;
   return ( data ) ;
-}
+} // end of getData
 
 async function ex3() {
   /* Train fashion images and test */
@@ -17,26 +24,31 @@ async function ex3() {
   nLabels = 10 ;
   bSize   = 64 ;     // Divvy up data to this many batches
 
-  model = tf.sequential ({
-    layers: [ tf.layers.dense ({units: imgSize, batchInputShape: [null, bSize]}), // input
-              tf.layers.dense ({units: 512, activation: 'relu'}),   // middle
-              tf.layers.dense ({units: nLabels, activation: 'softmax'})  // output
+ const model = tf.sequential ({
+    layers: [ tf.layers.dense ({units: imgSize, inputShape: [imgSize]}), // input
+              tf.layers.dense ({units: 512, activation: "relu"}),   // middle
+              tf.layers.dense ({units: nLabels, activation: "softmax"})  // output
             ]
   }); // end model
 
-  console.log (model.outputs[0].shape) ;
-  console.log (model.layers);
+
+  console.log ("Model Layers: \n", model.layers);
+  console.log ("Model Inputs  \n", model.inputs[0].shape);
+  console.log ("Model Outputs  \n", model.outputs[0].shape);
 
   // Let us train the model with data
-   trnData = await  getTrainData() ; 
+  trnData = await  getData("fashion/train.json") ; 
   x = tf.tensor2d (trnData.imgs) ;
-  lbls = tf.tensor2d (trnData.lbls, [60000, 1]) ;
+  y = tf.tensor2d (trnData.lbls) ;
 
-  model.compile ( {optimzer: "sgd", 
-                   loss: 'binaryCrossentropy'} );
+  const opt = tf.train.sgd (.01) ;
+  model.compile ({optimizer: opt,  loss: "categoricalCrossentropy"}) ;
 
-  const h = await
-       model.fit ( x, y, {batchSize: bSize, epochs: 3} );
-  console.log ("Loss after Epoch " + i + ": "
-                  + h.history.loss [0] ) 
-}
+  await model.fit (x, y, { batchSize: bSize, 
+	                   epochs:    3,
+                           callbacks: {
+                               onEpochEnd: async(epoch, logs) => {
+                                                 console.log ("Epoch: " + epoch +
+				                  " Loss: " + logs.loss); }
+                          }});
+ } // end of ex3
