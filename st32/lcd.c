53916896fc42f879730468448764f9f2764213d7
ravi6work <ravi6work@gmail.com>
2025-05-24 11:20:37 +1000

    Finally Got the Seg driver to work. The issue was TIM16 is linked to PA6 not TIM15, and even though it is not special Timer its MOE (Master output Enable flag) should be set for it to work. Got it from Gotchas on STM32 http://www.efton.sk/STM32/gotcha/gotchas.pdf

--- st32/lcd.c
+++ st32/lcd.c
@@ -1,22 +1,23 @@
-#include "stm32f3xx.h"
+#include "stm32f303x8.h"
 // Assumes default system clock = 8 MHz from HSI
 #define TIM2PSC 7    // 8000 / 8 =  1000kHz
-#define TIM2ARR 99   //  1000 / 100  (10kHz -> freq) 
-#define TIM3PSC 132  //  8000 / 133 = 60 kHz
-#define TIM3ARR 999  // 60 / 1000  kHz = (60 Hz -> freq)
+#define TIM2ARR 119  //  1000 / 120 (8.3kHz -> freq) 
+#define TIM3PSC 132  //  8300 / 133 = 63 kHz
+#define TIM3ARR 999  // 63 / 1000  kHz = (63 Hz -> freq)
 #define LED 4     // PB4 as LED indicator
 #define NUM_PHASES   4
 
 void setupLED(void) ;
 void blink (int n) ;
 void init_TIM2_PWM(void) ;
+void init_TIM16_PWM(void);
 void init_TIM3_IRQ(void) ;
 void TIM3_IRQHandler(void) ;
 
 volatile uint8_t phase = 0;
 
-// Duty cycles for 0%, 33%, 66%, %33 scaled to PWM_PERIOD
-const uint16_t pwmDuty[4] = {0, 33, 66, 33} ;
+// Duty cycles for 0%, 33%, 66%, %100 scaled to PWM_PERIOD
+const uint16_t pwmDuty[4] = {0, 40, 80, 120} ;
 
 // Each row = phase; each column = COMx duty
 const uint16_t comsTable[NUM_PHASES][4] = {
@@ -70,6 +71,13 @@ void TIM3_IRQHandler(void) {
         TIM2->CCR2 = comsTable[phase][1]; // COM1
         TIM2->CCR3 = comsTable[phase][2]; // COM2
         TIM2->CCR4 = comsTable[phase][3]; // COM3
+
+        uint8_t segState = 0b1010 ;  // for now static, assume COM0 is paired with it 
+        if ( (1 << phase) & segState ) {    // SEG Pin Output 
+          TIM16->CCR1 = TIM2ARR - comsTable[phase][0] ; // Complement Duty to turn ON
+          GPIOB->ODR ^= (1 << LED);  // Toggle PBx
+        }   
+        else TIM16->CCR1 = comsTable[phase][0] ; // Follow Common to turn OFF
         phase = (phase + 1) % NUM_PHASES;
     } 
 }
@@ -82,8 +90,8 @@ void init_TIM3_IRQ(void) {
     TIM3->ARR = TIM3ARR ;
 
     // Fire TIM3_IRQHandler every time Counter Overflow
-    TIM3->EGR = TIM_EGR_UG ; // Force update Event for reload register ARR
-    TIM3->SR = 0 ; // Clear Pending Flags (from status Register)
+    TIM3->EGR = TIM_EGR_UG ;    // Force update Event for reload register ARR
+    TIM3->SR = 0 ;              // Clear Pending Flags (from status Register)
     TIM3->DIER |= TIM_DIER_UIE; // Enable Update Interrupt
     NVIC_ClearPendingIRQ (TIM3_IRQn) ;
     NVIC_EnableIRQ (TIM3_IRQn);
@@ -110,19 +118,56 @@ void blink (int count) {
     }
 }
 
+void init_TIM16_PWM(void) {
+ // This timer is tied to PA6 ... can't move this
+ // Using CH1 of TIM16 as SIG pin  
+
+    RCC->AHBENR  |= RCC_AHBENR_GPIOAEN;
+    (void)RCC->AHBENR ;  // wait for above to completeyy
+    RCC->APB2ENR |= RCC_APB2ENR_TIM16EN;
+    (void)RCC->APB2ENR ;  // wait for above to completeyy
+
+    // TIM16 as PWM with output on  GPIOA PA6 as segment output 
+    // with AF1 alternate function
+      int i ;
+      i = 6 ;   // (PA6) as  segment pin
+      GPIOA->MODER &= ~(0x3 << (i * 2));     // Clear MODER_i
+      GPIOA->MODER |= (2 << (i * 2)) ;       // Set to AF mode
+      GPIOA->AFR[0] &= ~(0xF << (i * 4));    // Clear AFRL0 of pin_i
+      GPIOA->AFR[0] |=  (0x1 << (i * 4));    // set to AF1  function (TIM16)
+      GPIOA->OTYPER &= ~(1 << i);            // Push-pull
+      GPIOA->OSPEEDR |= (0x3 << (i * 2));    // High speed
+      GPIOA->PUPDR &= ~(0x3 << (i * 2));     // No pull-up/down
+
+    // Using same frequency settings as TIM2 that control Commons
+    TIM16->PSC = TIM2PSC ;
+    TIM16->ARR = TIM2ARR ;
+
+    // Channel_x  is active while  TIMx_CNT < TIMx_CCR1 else inactive
+    // Channel_x  output preload enable (TIMx_OC1PE)
+    TIM16->CCMR1 = (6 << 4) | (1 << 3) ;  // CH1 PWM mode 1 + preload 
+    TIM16->CCER |= TIM_CCER_CC1E ;
+    TIM16->CR1  |= TIM_CR1_ARPE;
+    TIM16->EGR   = TIM_EGR_UG;
+    // This is a must as TIM16 has complementary outputs
+    TIM16->BDTR |= TIM_BDTR_MOE ;   //Master output Enable
+    TIM16->CR1  |= TIM_CR1_CEN;
+}
 int main(void) {
 
   setupLED() ; // Debug indicator
+  GPIOB->ODR = (0 << LED);  // LED off
+
   init_TIM2_PWM() ; // used for common pins signals (4 off)
+  init_TIM16_PWM() ; // used for single SEG pin
   init_TIM3_IRQ() ;
 
    if (!(TIM2->CR1 & TIM_CR1_CEN))  // Timer is not  running!
         blink(3) ;
-   
-   if (!(RCC->APB1ENR & RCC_APB1ENR_TIM3EN) ||
-       !(TIM3->CR1 & TIM_CR1_CEN))  // TIM3 is not clocked or enabled
+   if (!(TIM16->CR1 & TIM_CR1_CEN))  // Timer is not  running!
+        blink(100) ;
+   if (!(TIM3->CR1 & TIM_CR1_CEN))  //  Timer not running
         blink(10) ;
-
     while (1) {
        __WFI();  // Sleep until interrupt
     }
