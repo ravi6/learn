b6f25a8c1ef98fb64420ade46b6123b4240af5ef
ravi6work <ravi6work@gmail.com>
2025-06-05 19:40:23 +1000

    Found out that segState 1st and 3rd indexed bits control two segments each. Did not find individual control as expeced. ChatGPT thinks it can still display all digits as desired ligting up both segments at a time. The trouble is both segments are controlled by (just one COM and one SEG.  That is the kicker. Cantsee how it will work

--- st32/lcd.c
+++ st32/lcd.c
@@ -1,15 +1,14 @@
 #include "stm32f303x8.h"
 // Assumes default system clock = 8 MHz from HSI
-#define TIM2SCL 2 
-#define TIM2CNT 256  //  8000/(2 * 256) =  (15.6kHz -> freq) 
-#define TIM3SCL 200 
-#define TIM3CNT 800 // 8000/(200*800)  kHz = (50 Hz -> freq)
+#define TIM2PSC 1
+#define TIM2ARR 255  //  8000/(2 * 256) =  (15.6kHz -> freq) 
+#define TIM3PSC 199 
+#define TIM3ARR 799 // 8000/(200*800)  kHz = (50 Hz -> freq)
 #define LED 4     // PB4 as LED indicator
 #define NUM_PHASES   4
 #define PWM_MODE1 6
 #define PWM_MODE2 7
 #define MIN(a, b) (((a) < (b)) ? ( a) : (b))
-
 
 void setupLED(void) ;
 void blink (int n) ;
@@ -20,26 +19,66 @@ void TIM3_IRQHandler(void) ;
 
 volatile uint8_t phase = 0;
 volatile uint8_t invert = 0;  // Com Table Inversion flag
+volatile uint8_t segState = 0b1000 ; //LSB to MSB give Seg On State in each phase
 
-/* Duty cycles for to PWM Count (Period)  (1/3 bias)
-const uint16_t pwmDuty[4] = {(TIM2CNT-1)/3,  (TIM2CNT-1)/2 , 2*((TIM2CNT-1)/3), (TIM2CNT-1)} ;
-
-// Each row = phase; each column = COMx duty
+//Duty cycles for to PWM Count (Period)  (slightly more than 1/3 bias)
+// Ideally [1/3, 1/2, 2/3, 1/2] .... typical 1/3 bias
+// But my LCD wants higher RMS ... 
+// This pattern has symmetry ie.  COM - (inv)COM =  [-1/3, 0, 1/3, 0]
+// In my case I scale this by 1.4 to get higher RMS
+//
+const float sf = 1.4 ;
+const uint16_t pwmDuty[4] = {TIM2ARR*(sf * 1/3), TIM2ARR*(sf * 1/2),
+                             TIM2ARR*(sf * 2/3), TIM2ARR} ;
 const uint16_t comsTable[NUM_PHASES][4] = {
-    { pwmDuty[0], pwmDuty[1], pwmDuty[2], pwmDuty[1] },
-    { pwmDuty[1], pwmDuty[2], pwmDuty[1], pwmDuty[0] },
-    { pwmDuty[2], pwmDuty[1], pwmDuty[0], pwmDuty[1] },
-    { pwmDuty[1], pwmDuty[0], pwmDuty[1], pwmDuty[2] }
+    { pwmDuty[0], pwmDuty[1], pwmDuty[2], pwmDuty[3] },
+    { pwmDuty[1], pwmDuty[2], pwmDuty[3], pwmDuty[0] },
+    { pwmDuty[2], pwmDuty[3], pwmDuty[0], pwmDuty[1] },
+    { pwmDuty[3], pwmDuty[0], pwmDuty[1], pwmDuty[2] }
 };
-*/
+void TIM3_IRQHandler(void) {
+   // Drive all com Lines setting voltage according to Mux phase
+   // This is achieved with duty cycle that is proportional to voltage ratio
 
-const uint16_t pwmDuty[4] = {0, (TIM2CNT-1)/3, 2*((TIM2CNT-1)/3), (TIM2CNT-1)} ;
-const uint16_t comsTable[NUM_PHASES][4] = {
-    { pwmDuty[0], pwmDuty[1], pwmDuty[2], pwmDuty[1] },
-    { pwmDuty[1], pwmDuty[2], pwmDuty[1], pwmDuty[0] },
-    { pwmDuty[2], pwmDuty[1], pwmDuty[0], pwmDuty[1] },
-    { pwmDuty[1], pwmDuty[0], pwmDuty[1], pwmDuty[2] }
-};
+    if (TIM3->SR & TIM_SR_UIF) {
+        TIM3->SR &= ~TIM_SR_UIF;
+
+
+        uint16_t comDuty ;
+
+           // Drive all COM lines for this phase
+           for (uint8_t com = 0 ; com < 4 ; com++) { 
+	      if (invert)  comDuty = pwmDuty[3] - comsTable[phase][com] ; 
+	      else comDuty = comsTable[phase][com] ;
+             
+              switch (com) {  // set CCR value corresponding to COM
+	 	case 0: TIM2->CCR1 = comDuty; break;  // COM0 
+	        case 1: TIM2->CCR2 = comDuty; break;  // COM1
+	        case 2: TIM2->CCR3 = comDuty; break;  // COM2
+	        case 3: TIM2->CCR4 = comDuty; break;  // COM3 
+              }
+            }
+
+            // Drive Segment Line that is connected to the active COM in this
+            // phase. Note active COM index is same as phase
+              uint16_t Cduty ;
+	      if (invert)  Cduty = pwmDuty[3] - comsTable[phase][phase] ; 
+	      else Cduty = comsTable[phase][phase] ;
+
+	      if ( (1 << phase) & segState ) {    // SEG Pin Output 
+		TIM16->CCR1 = pwmDuty[3] - Cduty ; // oppose comValue
+                GPIOB->ODR = (1 << LED);  // LED ON
+	      }   
+	      else {  
+		TIM16->CCR1 = Cduty; // follow com value 
+                GPIOB->ODR = (0 << LED);  // LED Off
+	      }
+
+        phase = (phase + 1) % NUM_PHASES;
+        if (phase == 0) invert = !invert ; // we flip coms at every segment update
+    } 
+} // End of TIM3_IRQ Handle
+
 void init_TIM2_PWM(void) {
     RCC->AHBENR  |= RCC_AHBENR_GPIOAEN;
     (void)RCC->AHBENR ;  // wait for above to completeyy
@@ -58,10 +97,10 @@ void init_TIM2_PWM(void) {
       GPIOA->PUPDR &= ~(0x3 << (i * 2));     // No pull-up/down
     }
 
-    TIM2->PSC = TIM2SCL - 1 ;
-    TIM2->ARR = TIM2CNT - 1 ;
+    TIM2->PSC = TIM2PSC ;
+    TIM2->ARR = TIM2ARR ;
 
-    // Channel_x  is active while  TIMx_CNT < TIMx_CCR1 else inactive
+    // Channel_x  is active while  TIMx_ARR < TIMx_CCR1 else inactive
     // Channel_x  output preload enable (TIMx_OC1PE)
     // PWM_MODE2   center aligned pwm  (Pulse count is at the middle of the pulse)
     // Improves alighnment
@@ -76,54 +115,12 @@ void init_TIM2_PWM(void) {
     TIM2->CR1  |= TIM_CR1_CEN;
 }
 
-void TIM3_IRQHandler(void) {
-   // Drive all com Lines setting voltage according to Mux phase
-   // This is achieved with duty cycle that is proportional to voltage ratio
-
-    if (TIM3->SR & TIM_SR_UIF) {
-        TIM3->SR &= ~TIM_SR_UIF;
-
-        uint8_t segState = 0b0001 ; //LSB to MSB give Seg On State in each phase
-        uint16_t comDuty ;
-
-           // Drive all COM lines for this phase
-           for (uint8_t com = 0 ; com < 4 ; com++) { 
-	      if (invert)  comDuty = pwmDuty[3] - comsTable[phase][com] ; 
-	      else comDuty = comsTable[phase][com] ;
-             
-              switch (com) {  // set CCR value corresponding to COM
-	 	case 0: TIM2->CCR1 = comDuty; break;  // COM0 
-	        case 1: TIM2->CCR2 = comDuty; break;  // COM1
-	        case 2: TIM2->CCR3 = comDuty; break;  // COM2
-	        case 3: TIM2->CCR4 = comDuty; break;  // COM3 
-              }
-            }
-
-            // Drive Segment Line that is connected to the active COM in this
-            // phase. Note active COM index is same as phase
-	      if (invert)  comDuty = pwmDuty[3] - comsTable[phase][phase] ; 
-	      else comDuty = comsTable[phase][phase] ;
-
-	      if ( (1 << phase) & segState ) {    // SEG Pin Output 
-		TIM16->CCR1 = pwmDuty[3] - comDuty ; // oppose comValue
-                GPIOB->ODR = (1 << LED);  // LED ON
-	      }   
-	      else {  
-		TIM16->CCR1 = comDuty; // follow com value 
-                GPIOB->ODR = (0 << LED);  // LED Off
-	      }
-
-        phase = (phase + 1) % NUM_PHASES;
-        if (phase == 0) invert = !invert ; // we flip coms at every segment update
-    } 
-} // End of TIM3_IRQ Handle
-
 void init_TIM3_IRQ(void) {
     RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
     (void)RCC->APB1ENR ;  // wait for above to completeyy
 
-    TIM3->PSC = TIM3SCL - 1 ;
-    TIM3->ARR = TIM3CNT - 1 ;
+    TIM3->PSC = TIM3PSC ;
+    TIM3->ARR = TIM3ARR ;
 
     // Fire TIM3_IRQHandler every time Counter Overflow
     TIM3->EGR = TIM_EGR_UG ;    // Force update Event for reload register ARR
@@ -147,10 +144,16 @@ void setupLED() {
     GPIOB->ODR |= (1 << LED);             // PBx = 1 (LED ON, if active high)
 }
 
+void delay (unsigned int time) {
+  // Time in MilliSeconds (Caliberated on STM32F303X8B)
+    for (unsigned int i = 0; i < time; i++)
+        for (volatile unsigned int j = 0; j < 575 ; j++);
+}
+
 void blink (int count) {
     for (int i = 0; i < count ; i++)  {
-        GPIOB->ODR ^= (1 << LED);  // Toggle PAx
-        for (volatile int i = 0; i < 200000; i++);  // crude delay
+        GPIOB->ODR ^= (3 << LED);  // Toggle PAx
+        delay (500) ;
     }
 }
 
@@ -176,10 +179,10 @@ void init_TIM16_PWM(void) {
       GPIOA->PUPDR &= ~(0x3 << (i * 2));     // No pull-up/down
 
     // Using same frequency settings as TIM2 that control Commons
-    TIM16->PSC = TIM2SCL - 1 ;
-    TIM16->ARR = TIM2CNT - 1 ;
+    TIM16->PSC = TIM2PSC  ;
+    TIM16->ARR = TIM2ARR  ;
 
-    // Channel_x  is active while  TIMx_CNT < TIMx_CCR1 else inactive
+    // Channel_x  is active while  TIMx_ARR < TIMx_CCR1 else inactive
     // Channel_x  output preload enable (TIMx_OC1PE)
     TIM16->CCMR1 = (PWM_MODE2 << 4) | (1 << 3) ;  // CH1 PWM mode 2 + preload 
     TIM16->CCER |= TIM_CCER_CC1E ;
@@ -205,6 +208,7 @@ int main(void) {
         blink(100) ;
    if (!(TIM3->CR1 & TIM_CR1_CEN))  //  Timer not running
         blink(10) ;
+
     while (1) {
        __WFI();  // Sleep until interrupt
     }
